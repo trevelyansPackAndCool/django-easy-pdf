af25c0a6bf28b79477c5ce020f89091ebf7ddb10
Michael May <michael@may.org.nz>
2018-06-26 16:59:52 +1200

    Added get_object() call if exists so DetailView views can work

--- easy_pdf/views.py
+++ easy_pdf/views.py
@@ -111,5 +111,6 @@ class PDFTemplateView(PDFTemplateResponseMixin, ContextMixin, View):
         """
         Handles GET request and returns HTTP response.
         """
-        context = self.get_context_data(**kwargs)
+        self.object = self.get_object() if hasattr(self, 'get_object') else None
+        context = self.get_context_data(object=self.object, **kwargs)
         return self.render_to_response(context)
